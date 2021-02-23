5744c033d181498da858034080995ab56b4f641c
Isaac Kelly <isaacmkelly0@gmail.com>
2021-02-23 12:49:38 -0600

    edited files referencing App

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import './Routes.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+
+const Routes = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default Routes;
